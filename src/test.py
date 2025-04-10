5aa67418d1fa725f8c4a99d0a6fb37d8959529ef
Rodion Marochkin <rodionmarochkin32@gmail.com>
2025-04-10 10:38:35 +0300

    test

--- src/test.py
+++ src/test.py
@@ -1,4 +1,9 @@
-print("123")
-print("123")
-print("123")
+def single_number(nums: list) -> int:
+    xor_res = 0
+    for el in nums:
+        xor_res ^= el
+    return xor_res
 
+
+numbers = [4, 3, 2, 3, 2]
+print(single_number(numbers))
